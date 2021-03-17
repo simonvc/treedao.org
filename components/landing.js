1cd1b82ecd64d2425d15fb25f45141290b0ff5e4
Simon Vans-Colina <simon@fronted.xyz>
2021-03-17 21:23:47 +0000

    tree -> NFTree

--- components/landing.js
+++ components/landing.js
@@ -3,7 +3,7 @@ const Landing = () => {
     <div className="w-full flex flex-grow">
       <div className="container px-4 py-12 md:py-24 mx-auto flex flex-col items-center justify-around text-center text-3xl md:text-4xl lg:text-5xl leading-9 font-bold md:font-extrabold text-navyBlue w-full">
         <h2 className="my-4">
-          Why send flowers when you can send a whole tree?
+          Why send flowers when you can send a whole NFTree?
         </h2>
 
         <div className="my-4 text-xl md:text-2xl lg:text-3xl font-normal">
