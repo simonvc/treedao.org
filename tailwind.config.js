98b0577c0bc9be8f3ad0444cd16426cad3fa4440
Kate Beard <asouthernbellein@gmail.com>
2021-03-18 20:29:57 +0000

    Update npm packages and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
           sans: ["Roboto", "Archivo Black"],
         },
       },
+      // transitionProperty: {
+      //   width: "width",
+      // },
+      // transitionDuration: {
+      //   2000: "2000ms",
+      //   2250: "2250ms",
+      //   4000: "4000ms",
+      //   4250: "4250ms",
+      //   6000: "6000ms",
+      //   6250: "6250ms",
+      // },
+      // transitionTimingFunction: {
+      //   "ease-in-out-slowed": "cubic-bezier(0.3, 0.2, 1, 1)",
+      // },
+      boxShadow: {
+        "md-top":
+          "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
+      },
       colors: {
         navyBlue: "#2f4858",
         darkGreen: "#3f4a3c",
